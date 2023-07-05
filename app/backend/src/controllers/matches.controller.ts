0eac2a2e91dc2bfbf096030318ff1ded95b3eb7b
joao <joao.dev94@gmail.com>
2023-07-05 17:42:50 -0300

    Requisitos 15 e 16 - Adicão de requisicoes para rota /matches

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -2,7 +2,17 @@ import { Request, Response } from 'express';
 import MatchService from '../services/matches.service';
 
 export default class TeamController {
-  static async getMatches(_req: Request, res: Response) {
+  static async getMatches(req: Request, res: Response) {
+    const { inProgress } = req.query;
+    if (inProgress === 'true') {
+      console.log('entrou no if');
+      const inProgressMatches = await MatchService.getInProgressMatches();
+      return res.status(200).json(inProgressMatches);
+    }
+    if (inProgress === 'false') {
+      const finishedMatches = await MatchService.getFinishedMatches();
+      return res.status(200).json(finishedMatches);
+    }
     const allMatches = await MatchService.getMatches();
     return res.status(200).json(allMatches);
   }
--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -12,4 +12,26 @@ export default class MatchService {
     });
     return matches;
   }
+
+  static async getInProgressMatches(): Promise<IMatch[] | null> {
+    const inProgressMatches = await MatchesModel.findAll({
+      where: { inProgress: true },
+      include: [
+        { model: TeamModel, as: 'homeTeam' },
+        { model: TeamModel, as: 'awayTeam' },
+      ],
+    });
+    return inProgressMatches;
+  }
+
+  static async getFinishedMatches(): Promise<IMatch[] | null> {
+    const finishedMatches = await MatchesModel.findAll({
+      where: { inProgress: false },
+      include: [
+        { model: TeamModel, as: 'homeTeam' },
+        { model: TeamModel, as: 'awayTeam' },
+      ],
+    });
+    return finishedMatches;
+  }
 }
